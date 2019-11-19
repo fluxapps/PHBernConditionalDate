3a5aab59bf6a10459252e3f2e83127f52bac7dbf
sragfw <fw@studer-raimann.ch>
2019-11-19 09:37:27 +0100

    update plugin.php for 5.4

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 $id = 'phbe_cdate';
 $version = '1.0.0';
 $ilias_min_version = '5.2.000';
-$ilias_max_version = '5.2.999';
+$ilias_max_version = '5.4.999';
 $responsible = 'Theodor Truffer';
 $responsible_mail = 'tt@studer-raimann.ch';
 ?>
