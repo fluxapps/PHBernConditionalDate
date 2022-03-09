496998be3aa1460d36e7bdf52ed6e7bf53fb10f8
fluxms <martin@fluxlabs.ch>
2022-03-08 22:03:09 -0400

    updated plugin.php

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 $id = 'phbe_cdate';
-$version = '1.0.0';
+$version = '1.1.0';
 $ilias_min_version = '6.0';
-$ilias_max_version = '6.99';
+$ilias_max_version = '7.99';
 $responsible = 'Theodor Truffer';
 $responsible_mail = 'tt@studer-raimann.ch';
 ?>
